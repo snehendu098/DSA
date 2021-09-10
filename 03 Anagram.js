1bc7c0ad05e66793eaf2654937c951e73f8c9190
Snehendu Roy <roysnehendupersonal@gmail.com>
2021-09-11 02:36:58 +0530

    String Anagram problem

--- 03 Anagram.js
+++ 03 Anagram.js
@@ -1,3 +1,34 @@
-// String anagram
-// Input : "hello"
-// Output: "llleo"
+// String anagram -> If we have two strings with same number of letters and characters, then it forms an anagram.
+
+// Input : "hello" -> "llheo"
+
+// conditions
+// check the length of both the strings.
+// String "hello"
+// {h: 1, e: 1, l: 2, o: 2}
+// we wi;; decrement the number of letters whil;e checking the second string
+
+function isAnagram(string1, string2) {
+  if (string1.length !== string2.length) {
+    return false;
+  }
+
+  var counter = {};
+
+  for (let letter of string1) {
+    counter[letter] = (counter[letter] || 0) + 1; // counter["h"] = 1 (for the first time)
+    // console.log(counter[letter]);
+  }
+
+  for (let items of string2) {
+    if (!counter[items]) {
+      console.log(items);
+      return false;
+    }
+    counter[items] -= 1;
+  }
+  //   console.log(counter);
+  return true;
+}
+
+console.log(isAnagram("hello", "llheo"));
